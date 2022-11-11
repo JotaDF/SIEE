8f9a89389fa5266a93f10310b4243f5b2aa40b72
Alessandro <alessandrocosta103@gmail.com>
2022-11-11 12:32:43 -0300

    cria classe coordenador

---
+++ projeto/model/Coordenador.php
@@ -0,0 +1,27 @@
+<?php
+
+class Coordenador {
+   
+    var $id;
+    var $cpf;
+    var $nome;
+    var $graduacao;
+    var $pos_graduacao;
+    var $telefone;
+    var $email;
+    var $nucleo;
+    var $turno;
+    var $id_curso;
+    var $sena;
+    
+    public function __construct() {
+        
+    }
+
+    // falta gets e sets 
+
+
+
+
+  
+}
