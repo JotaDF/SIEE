8df4fdaf61c32c888b451cbf909e00fa0eb66d23
Alessandro <alessandrocosta103@gmail.com>
2022-11-12 01:33:32 -0300

    cria classe Coordenador

--- projeto/model/Coordenador.php
+++ projeto/model/Coordenador.php
@@ -1,7 +1,8 @@
 <?php
 
-class Coordenador {
-   
+class Coordenador
+{
+
     var $id;
     var $cpf;
     var $nome;
@@ -12,16 +13,98 @@ class Coordenador {
     var $nucleo;
     var $turno;
     var $id_curso;
-    var $sena;
-    
-    public function __construct() {
-        
+    var $senha;
+
+    public function __construct(){
+
     }
 
-    // falta gets e sets 
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getCpf(){
+        return $this->cpf;
+    }
 
 
+    public function getNome(){
+        return $this->nome;
+    }
 
+    public function getGraduacao(){
+        return $this->graduacao;
+    }
 
-  
+    public function getPos_graduacao(){
+        return $this->pos_graduacao;
+    }
+
+    public function getTelefone(){
+        return $this->telefone;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getNucleo(){
+        return $this->nucleo;
+    }
+
+    public function getTurno(){
+        return $this->turno;
+    }
+
+    public function getId_curso(){
+        return $this->id_curso;
+    }
+
+    public function getSenha(){
+        return $this->senha;
+    }
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function setCpf($cpf){
+        $this->cpf = $cpf;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    public function setGraduacao($graduacao){
+        $this->graduacao = $graduacao;
+    }
+
+    public function setPos_graduacao($pos_graduacao){
+        $this->pos_graduacao = $pos_graduacao;
+    }
+
+    public function setTelefone($telefone){
+        $this->telefone = $telefone;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function setNucleo($nucleo){
+        $this->nucleo = $nucleo;
+    }
+
+    public function setTurno($turno){
+        $this->turno = $turno;
+    }
+
+    public function setId_curso($id_curso){
+        $this->id_curso = $id_curso;
+    }
+
+    public function setSenha($senha){
+        $this->senha = $senha;
+    }
 }
